4bc6df6540e06bfb37a2332f80e3f1cbd225095c
Eihel <51751564+Eihel@users.noreply.github.com>
2019-11-26 17:51:33 +0100

    Update index.php

--- wikidata-externalid-url/index.php
+++ wikidata-externalid-url/index.php
@@ -1,5 +1,4 @@
 <?PHP
-
 // Copyright 2016,2017 Arthur P. Smith
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,12 +18,11 @@
 $property = isset($_REQUEST['p']) ? $_REQUEST['p'] : '' ;
 $url_prefix = isset($_REQUEST['url_prefix']) ? $_REQUEST['url_prefix'] : '' ;
 $url_suffix = isset($_REQUEST['url_suffix']) ? $_REQUEST['url_suffix'] : '' ;
-$id = isset($_REQUEST['id']) ? $_REQUEST['id'] : '' ;
+$url = rawurldecode(isset($_REQUEST['url']) ? $_REQUEST['url'] : '') ;
+$exp = rawurldecode(isset($_REQUEST['exp']) ? $_REQUEST['exp'] : '');
+$id = rawurldecode(isset($_REQUEST['id']) ? $_REQUEST['id'] : '') ;
 
 if (! empty($id) ) {
- if (strpos($id, '%' !== FALSE)) {
-    $id = urldecode($id);
- }
  switch($property) {
   case 213: // ISNI
     $link_string = str_replace(" ", "", $id) ;
@@ -102,6 +100,10 @@ if (! empty($id) ) {
     $vat_digits = substr($id, 2);
     $link_string = "$member_state_code&number=$vat_digits";
     break;
+  case 3723: // USCG Lighthouse ID
+    preg_match('/(\d+)-(\d+(?:.\d*)?)/', $id, $a);
+    $link_string = "https://msi.nga.mil/queryResults?publications/uscgll?volume=".$a[1]."&featureNumber=".$a[2]."&includeRemovals=false&output=html";
+    break;    
   case 4033: // Mastodon address
     $mastodon_parts = split("@", $id);
     $m_user = $mastodon_parts[0];
@@ -135,18 +137,51 @@ if (! empty($id) ) {
     $id = $itf_parts[1];
     $link_string = "https://www.itftennis.com/procircuit/tournaments/$sex's-tournament/info.aspx?tournamentid=$id";
     break;
+  case 6996: // Epitafier.se ID
+    switch(substr($id, 0, 1)) {
+      case 'B':
+        $link_string = "http://www.epitafier.se/?page=begravningsvapen&subj=$id";
+        break;
+      case 'E':
+        $link_string = "http://www.epitafier.se/?page=epitafier&subj=$id";
+        break;
+      default:
+        $link_string = "http://www.epitafier.se/?page=begravningsvapen&subj=$id" ;
+        break;
+    }
+    break;
+  case 1695: // NLP ID
+    switch(substr($id, 0, 1)) {
+      case 'A':
+        $link_string = "http://mak.bn.org.pl/cgi-bin/KHW/makwww.exe?BM=1&NU=1&IM=5&WI=$id" ;
+        break;
+      case '9':
+        $link_string = "http://mak.bn.org.pl/cgi-bin/KHW/makwww.exe?BM=1&NU=1&IM=4&WI=$id" ;
+        break;
+    }
+    break;
   default:
-    $link_string = $id ;
+    if (! empty($exp) ) {
+      preg_match('/'.$exp.'/', $id, $a);
+      for($i=0; $i<sizeof($a); $i++) {
+        $url = str_replace("%".$i, $a[$i], $url);
+      }
+      $link_string = $url;
+    }
+    else
+      $link_string = $id ;
+    break;
+    
+
     break ;
  }
 
  $redirect_url = $url_prefix . $link_string . $url_suffix ;
-
  header("Location: $redirect_url");
  exit();
 }
 
-print "<html><head><title>Wikidata External ID redirector</title></head>" ;
+print "<html><head><meta charset='utf-8'><title>Wikidata External ID redirector</title></head>" ;
 print "<body><h1>Wikidata External ID redirector</h1>" ;
 print "This accepts the following '?' parameters and returns a redirect constructed from them:" ;
 print "<ul>" ;
@@ -156,9 +191,24 @@ print "<li> url_suffix - eg. .html</li>";
 print "<li> id - the id value of this external id property for an entity of interest</li>";
 print "</ul>";
 
+$url   = "http://test.org/?vol=%1&item=%2";
+$id    = "113-1250";
+$exp = "(.*)-(.*)";
+
+print "This accepts the alternative '?' parameters:" ;
+print "<ul>" ;
+print "<li> url - url with parameters %1, %2... eg. <i>".$url."</i></li>";
+print "<li> exp - regular expression eg. <i>".$exp."</i></li>";
+print "<li> id - eg. <i>".$id."</i></li>";
+$p = "?url=".urlencode($url)."&exp=".urlencode($exp)."&id=".urlencode($id);
+print "<li> example <a href='index.php".$p."'>http://tools.wmflabs.org/wikidata-externalid-url/".$p."</a></li>";
+$r = "http://test.org/?vol=113?item=1250";
+print "<li> result  <a href='".$r."'>".$r."</a></li>";
+print "</ul>";
+print "Note: all parameters should be url encoded.<br/>";
 print "Note: this script also URL-decodes the id value so that an id with several embedded parameters can be used as originally intended.";
 
-print "<p>An example: <a href=\"http://tools.wmflabs.org/wikidata-externalid-url/?p=213&url_prefix=http://isni.org/&id=0000 0000 8045 6315\">http://tools.wmflabs.org/wikidata-externalid-url/?p=213&url_prefix=http://isni.org/&id=0000 0000 8045 6315</a>.</p>";
+print "<p>An example: <a href=\"index.php?p=213&url_prefix=http://isni.org/&id=0000 0000 8045 6315\">http://tools.wmflabs.org/wikidata-externalid-url/?p=213&url_prefix=http://isni.org/&id=0000 0000 8045 6315</a>.</p>";
 
 print "<p>Currently supported id translations:</p>";
 print "<ul>";
@@ -171,11 +221,14 @@ print "<li>CN - property 1209</li>";
 print "<li>TA98 - property 1323</li>";
 print "<li>CricketArchive - property 2698</li>";
 print "<li>EU VAT number - property 3608</li>";
+print "<li>USCG Lighthouse ID - property 3723</li>";
 print "<li>Mastodon - property 4033</li>";
 print "<li>UOL Brazil election id - property 5892</li>";
 print "<li>Swedish Organization Number - property 6460</li>";
 print "<li>Gamepedia article ID - property 6623</li>";
 print "<li>ITF tournament ID - property 6841</li>";
+print "<li>Epitafier.se ID - property 6996</li>";
+print "<li>NLP ID - property 1695</li>";
 print "</ul>";
 
 print "The <a href=\"https://github.com/arthurpsmith/wikidata-tools/tree/master/wikidata-externalid-url\">source code for this service</a> is available under the <a href=\"http://www.apache.org/licenses/LICENSE-2.0\">Apache License, Version 2.0</a>." ;
